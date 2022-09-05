7673533deece63e3eda1b07bf145287b9e7698fd
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2022-09-05 19:25:05 +0200

    laver udskriv fra starten

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,32 @@ public class Main {
 	// write your code here
 
 
+        Node node1 = new Node("hej");
+        Node node2 = new Node("med");
+        Node node3 = new Node("dig");
 
+        node1.next = node2;
+        node2.next = node3;
+
+        node3.previous = node2;
+        node2.previous = node1;
+
+
+        Node n = node1;
+
+        printFromhead(n);
+
+
+    }
+
+    private static void printFromhead(Node n)
+    {
+        while (n != null) {
+
+            System.out.println(n.data);
+
+            n = n.next;
+        }
     }
 
 
